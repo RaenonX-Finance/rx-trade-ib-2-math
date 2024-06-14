332eb1e72421b43bae91b2f2dffcda1ce3a8bde2
RaenonX <raenonx0710@gmail.com>
2024-06-14 06:32:57 -0500

    Fixed gamma field possibly returning incorrect result
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- rx_trade_ib_2/options/gex/gamma_field.py
+++ rx_trade_ib_2/options/gex/gamma_field.py
@@ -10,7 +10,10 @@ def calc_gamma_field(
 ) -> float | None:
     gamma_field_idx = np.argmin(total_gamma)
 
-    if np.min(total_gamma) == total_gamma[0]:
+    min_net_gamma = np.min(total_gamma)
+
+    # Min at the very left or the right, likely not a valid field
+    if min_net_gamma == total_gamma[0] or min_net_gamma == total_gamma[len(total_gamma) - 1]:
         return None
 
     return cast(float, price_levels[gamma_field_idx])
