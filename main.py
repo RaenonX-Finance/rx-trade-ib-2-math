37c32ddeff41153875386679e92e09b2e86d92e2
RaenonX <raenonx0710@gmail.com>
2024-06-14 06:07:41 -0500

    Added CORS for API
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from rx_trade_ib_2.options.gex.main import calc_gex_stats
 from rx_trade_ib_2.options.gex.request import OptionsGexStatsRequest
 from rx_trade_ib_2.options.gex.response import OptionsGexStatsResponse
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:3100",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/options/gex", response_model=OptionsGexStatsResponse)
